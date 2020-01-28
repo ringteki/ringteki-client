1aa0a827eb7dc41164ac728de1a42c6822bd2e4f
gryffon <ryan@karetas.com>
2020-01-28 18:50:21 -0500

    Remove server code from client repo

--- server/game/CardSelectors/UnlimitedCardSelector.js
+++
@@ -1,9 +0,0 @@
-const BaseCardSelector = require('./BaseCardSelector.js');
-
-class UnlimitedCardSelector extends BaseCardSelector {
-    hasReachedLimit() {
-        return false;
-    }
-}
-
-module.exports = UnlimitedCardSelector;
